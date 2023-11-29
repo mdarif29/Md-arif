95d815f2426f9331c481748fbca9b02ce6f6cac0
mdarif29 <54501188+mdarif29@users.noreply.github.com>
2023-11-29 21:29:28 +0530

    Create Binary-search.go

---
+++ Binary-search.go
@@ -0,0 +1,88 @@
+
+here is a step-by-step algorithm for performing a binary search:
+
+Input:
+
+A sorted array arr of size n.
+A target element x that we want to search for in the array.
+Output:
+
+The index of the target element in the array, or -1 if it is not present.
+Step 1: Initialize variables
+
+Let low be 0, which will represent the lower bound of the search interval.
+Let high be n-1, which will represent the upper bound of the search interval.
+Let mid be (low + high) / 2, which will represent the middle index of the search interval.
+Step 2: Compare the target element with the element at the middle index
+
+If arr[mid] is equal to x, then the target element has been found, and the search can stop. Return the index mid as the result.
+Otherwise, if arr[mid] is less than x, then the target element must be in the lower half of the search interval, so set high to mid-1 and repeat the comparison.
+Otherwise, if arr[mid] is greater than x, then the target element must be in the upper half of the search interval, so set low to mid+1 and repeat the comparison.
+Step 3: Repeat steps 1 and 2 until the target element is found or the search interval is empty
+
+Continue repeating steps 1 and 2 until the target element is found or the search interval is empty (i.e., low > high).
+If the search interval becomes empty before the target element is found, then the element is not present in the array, and the search can return -1 as the result.
+Example of how the binary search algorithm works:
+
+Suppose we have the following sorted array:
+arr = [2, 5, 8, 12, 16, 23, 38, 56, 72, 91]
+And suppose we want to search for the element 23 in the array. Here's how the binary search algorithm would work:
+
+Step 1: Initialize variables
+
+low = 0
+high = 9
+mid = 4
+Step 2: Compare the target element with the element at the middle index
+
+arr[mid] = 16
+Since arr[mid] is greater than the target element 23, set low to mid+1 = 5 and repeat the comparison.
+Step 2 (again): Compare the target element with the element at the middle index
+
+arr[mid] = 23
+Since arr[mid] is equal to the target element, the search can stop. Return the index mid = 5 as the result.
+The final result is that the element 23 is found at index 5 in the array.
+
+Time complexity analysis:
+Each iteration of the binary search algorithm reduces the size of the search interval by half, so the number of iterations required to find the target element is logarithmic in the size of the array. 
+Specifically, the time complexity of the binary search algorithm is O(log n), where n is the size of the array.
+
+//WAP OF BINARY SEARCH 
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func binarySearch(arr []int, x int) int {
+	low := 0
+	high := len(arr) - 1
+	var mid int
+
+	for low <= high {
+		mid = (high + low) / 2
+
+		if arr[mid] < x {
+			low = mid + 1
+		} else if arr[mid] > x {
+			high = mid - 1
+		} else {
+			return mid
+		}
+	}
+
+	return -1
+}
+
+func main() {
+	arr := []int{2, 3, 4, 10, 40}
+	x := 10
+
+	result := binarySearch(arr, x)
+	if result != -1 {
+		fmt.Printf("Element is present at index %d", result)
+	} else {
+		fmt.Printf("Element is not present in array")
+	}
+}
